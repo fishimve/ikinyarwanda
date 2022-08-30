c76ed9fb68b19c07b4aa7a37b01ed68ae4f77cc1
Fab Ishimve <fabriceishimwe98@gmail.com>
2022-08-30 22:46:10 +0300

    new icon

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\fabri\Desktop\ikinyarwanda"
+export "FLUTTER_APPLICATION_PATH=C:\Users\fabri\Desktop\Archive\ikinyarwanda"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
